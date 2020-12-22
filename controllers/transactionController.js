ce59a254bf55bd33d200082f509ad73394e1fcc9
carneirosarah <sarahrlcarneiro@gmail.com>
2020-12-21 22:58:31 -0300

    adição de rotas transações por data

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -46,7 +46,6 @@ exports.getByUser = async (req, res, next) => {
 
         return res.status(500).send({error: error})
     }
-
 }
 
 exports.totalReceivedByUser = async (req, res, next) => {
@@ -151,5 +150,83 @@ exports.delete = async (req, res, next) => {
     } catch (error) {
 
         return res.status(401).send({ message: 'Delete Failure', status: 401})
+    }
+}
+
+exports.getByUserAndPeriod = async (req, res, next) => {
+
+    try {
+
+        let result = await mysql.execute('SELECT * FROM transaction where id_user = ? and date_transaction between ? and ?', 
+            [req.params.id_user, req.params.date_begin, req.params.date_finish])
+            
+            response = {
+                transactions: result
+            }
+    
+            return res.status(201).send(response)
+
+    } catch (error) {
+
+        return res.status(500).send({error: error})
+    }
+}
+
+exports.totalReceivedByUserAndPeriod = async (req, res, next) => {
+
+    try {
+
+        let result = await mysql.execute('SELECT sum(value_transaction) as \'valor_total_recebido\' FROM transaction where id_user = ? and type_transaction=\'R\' and date_transaction between ? and ? group by id_user', 
+            [req.params.id_user, req.params.date_begin, req.params.date_finish])
+            
+            response = {
+                transaction: result
+            }
+    
+            return res.status(201).send(response)
+
+    } catch (error) {
+
+        return res.status(500).send({error: error})
+    }
+}
+
+exports.totalSpentByUserAndPeriod = async (req, res, next) => {
+
+    try {
+
+        let result = await mysql.execute('SELECT sum(value_transaction) as \'valor_total_gasto\' FROM transaction where id_user = ? and type_transaction=\'D\' and date_transaction between ? and ? group by id_user', 
+            [req.params.id_user, req.params.date_begin, req.params.date_finish])
+            
+            response = {
+                transaction: result
+            }
+    
+            return res.status(201).send(response)
+
+    } catch (error) {
+
+        return res.status(500).send({error: error})
+    }
+}
+
+exports.balanceByUserAndPeriod = async (req, res, next) => {
+
+    try {
+
+        let result = await mysql.execute('SELECT (valor_total_recebido - valor_total_gasto) as saldo FROM ' + 
+            '(select id_user, sum(value_transaction) as \'valor_total_recebido\' from `poupador`.`transaction` where id_user = ? and type_transaction=\'R\' and date_transaction between ? and ? group by id_user) as t1 ' +
+            'join (select id_user, sum(value_transaction) as \'valor_total_gasto\' from `poupador`.`transaction` where id_user = ? and type_transaction=\'D\' and date_transaction between ? and ? group by id_user) as t2 ' +
+            'on t1.id_user = t2.id_user', [req.params.id_user, req.params.date_begin, req.params.date_finish, req.params.id_user, req.params.date_begin, req.params.date_finish])
+            
+            response = {
+                transaction: result
+            }
+    
+            return res.status(201).send(response)
+
+    } catch (error) {
+
+        return res.status(500).send({error: error})
     }
 }
